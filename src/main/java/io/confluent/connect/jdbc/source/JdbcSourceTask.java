c423494f4f9ab10ae8fdc99eee9d612414d714bd
tanmayachugh <tanmayachugh@gmail.com>
2021-07-12 15:48:58 +0530

    Closes backing store when connector is stopped

--- src/main/java/io/confluent/connect/jdbc/source/JdbcSourceTask.java
+++ src/main/java/io/confluent/connect/jdbc/source/JdbcSourceTask.java
@@ -343,6 +343,7 @@ public class JdbcSourceTask extends SourceTask {
   public void stop() throws ConnectException {
     log.info("Stopping JDBC source task");
     running.set(false);
+    backingStore.close();
     // All resources are closed at the end of 'poll()' when no longer running or
     // if there is an error
   }
